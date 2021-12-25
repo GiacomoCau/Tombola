d49872d737431354e67eac3c9d7b743e09e47845
Giacomo <Cau.Giacomo190@Tiscali.it>
2021-12-25 12:58:22 +0100

    ottimizzato R

--- Core.js
+++ Core.js
@@ -2,16 +2,13 @@
 	function R(rs, sm, z) {
 		// rs: elementi per riga
 		// z[]: elementi per colonna
-		return R(rs, sm, z, [])
+		return R(0, rs, [])
 		
-		function* R(rs, sm, z, v) {
-			if (z.length == 0)
+		function* R(i, rs, v) {
+			if (i == z.length)
 				yield v
-			else for (var n=rs>=z.length||sm[0]===0?1:0, e=rs&&z[0]?1:0; n<=e; n+=1) {
-				yield* R(rs-n, shift(sm), shift(z), v.concat(n))
-			}
-			function shift(v) { // clona v[] dal secondo elemento
-				return v.slice(1)
+			else for (var n=rs>=z.length-i||sm[i]===0?1:0, e=rs&&z[i]?1:0; n<=e; n+=1) {
+				yield* R(i+1, rs-n, v.concat(n))
 			}
 		}
 	}
@@ -31,8 +28,8 @@
 			function sum(m) {
 				var r=[]; for (var v of m) for (var i in v) r[i]=(r[i]||0)+v[i]; return r
 			}
-			function subv(a, b) { // clona a[] sottraendo agli elementi b[]
-				var r=a.slice(0); for (var i in a) r[i]-=b[i]; return r
+			function subv(z, v) { // clona a[] sottraendo agli elementi b[]
+				var r=z.slice(0); for (var i in z) r[i]-=v[i]; return r
 			}	
 		}
 	}
