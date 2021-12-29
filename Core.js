4630080ee70878512c46dc9ab5a531f5933f7c6b
Giacomo <Cau.Giacomo190@Tiscali.it>
2021-12-29 16:27:37 +0100

    sostituiti i for in, definito controllo totali riga == totali colonna

--- Core.js
+++ Core.js
@@ -5,21 +5,35 @@
 		// tr: totale per riga
 		// tc[]: totali per colonna
 		// sc[]: somme per colonna
+		//if (tr != tc.reduce((a,b)=>a+b)-sc.reduce((a,b)=>a+b)) throw 'T: totali riga != totali colonna'
 		return R(0, tr, [])
 		
 		function* R(i, tr, r) {
-			if (i == tc.length)
+			if (i == tc.length) // i ~ r.length
 				yield r
 			else for (var n=tr>=tc.length-i||sc[i]===0?1:0, e=tr&&tc[i]?1:0; n<=e; n+=1) {
+			//else for (var n=tr<tc.length-i&&sc[i]!=0?0:1, e=tr&&tc[i]?1:0; n<=e; n+=1) {
 				yield* R(i+1, tr-n, r.concat(n))
 			}
 		}
+		/* TODO in sostituzione del precedente
+		return R(tr, [])
+		
+		function* R(tr, r) {
+			if (r.length == tc.length)
+				yield r
+			else for (var n=tr>=tc.length-r.length||sc[r.length]===0?1:0, e=tr&&tc[r.length]?1:0; n<=e; n+=1) {
+				yield* R(tr-n, r.concat(n))
+			}
+		}
+		*/
 	}
 	
 	function M(nr, tr, tc) {
 		// nr: righe per scheda
 		// tr: totali per riga
 		// tc[]: totali per colonna
+		//if (nr*tr != tc.reduce((a,b)=>a+b)) console.log('T: totali riga != totali colonna')
 		return M(tc, [])
 		
 		function* M(tc, m) {
@@ -29,10 +43,10 @@
 				yield* M(subr(tc, r), m.concat([r]))
 			}
 			function sumc(m) { // somma le colonne di m
-				var r=[]; for (var v of m) for (var i in v) r[i]=(r[i]||0)+v[i]; return r
+				var r=[]; for (var v of m) for (var i=0; i<v.length; i+=1) r[i]=(r[i]||0)+v[i]; return r
 			}
 			function subr(tc, r) { // clona tc[] sottraendo agli elementi gli elementi di r[]
-				tc=tc.slice(0); for (var i in tc) tc[i]-=r[i]; return tc
+				tc=tc.slice(0); for (var i=0; i<tc.length; i+=1) tc[i]-=r[i]; return tc
 			}	
 		}
 	}
@@ -42,6 +56,7 @@
 		// nr: numero righe per scheda
 		// tr: totale per riga
 		// tc[]: totali per colonna
+		if (ns*nr*tr != tc.reduce((a,b)=>a+b)) console.log('T: totali riga != totali colonna')
 		return T(tc, [])
 		
 		function* T(tc, t) {
@@ -51,10 +66,11 @@
 				yield* T(subm(tc, m), t.concat([m]))
 			}	
 			function subn(tc, n) { // clona tc[] sottranedo agli elementi n
-				tc=tc.slice(0); for (var i in tc) tc[i]-=n; return tc
+				tc=tc.slice(0); for (var i=0; i<tc.length; i+=1) tc[i]-=n; return tc
 			}
 			function subm(tc, m) { // clona tc[] sottranedo agli elementi le colonne di m[,]
-				tc=tc.slice(0); for (var r of m) for (var i in r) tc[i]-=r[i]; return tc
+				tc=tc.slice(0); for (var r of m) for (var i=0; i<r.length; i+=1) tc[i]-=r[i]; return tc
 			}
 		}
 	}
+
