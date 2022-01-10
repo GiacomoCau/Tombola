7f36270ca38bac591dbff9d93e6d7e49dfb01466
Giacomo <Cau.Giacomo190@Tiscali.it>
2022-01-10 16:55:34 +0100

    aggiunte variabili d'ambiente

--- SetNode.bat
+++ SetNode.bat
@@ -1,2 +1,5 @@
 set NODE_DISABLE_COLORS=1
+set NODE_SKIP_PLATFORM_CHECK=1
 path D:\Programmi\Node-v13.14.0-win-x64;%path%
+set NODE_PATH=D:\Programmi\Node-v13.14.0-win-x64\node_module
+rem path D:\Programmi\Nodejs;C:\Users\Giacomo\AppData\Roaming\npm;%path%
