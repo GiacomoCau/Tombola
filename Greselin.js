1dcfcb6dafd95267645eac82988f4983b1ffe1cc
Giacomo <Cau.Giacomo190@Tiscali.it>
2022-03-14 11:03:19 +0100

    raffinata la presentazione rivisti metodi M, Mn, R

--- Greselin.js
+++ Greselin.js
@@ -1,3 +1,4 @@
+
 var min = Math.min, max = Math.max
 
 function sum(v) { // somma tutti gli elementi di v
@@ -22,89 +23,75 @@ function subn(c, a) { // crea un vettore con la differenza fra i corrispondenti 
 
 function R(mx, r, c) {
 	var r1 = sum1(mx, r)
-	return R(r[0], c)
+	return R(r[0], c, [])
 	
-	function R(r0, c) {
+	function* R(r0, c, v) {
 		if (c.length == 2) {
-			for (var v=[], a0=max(0,r0-mx,c[0]-r1); a0<=mx; a0+=1) {
-				var a1 = r0 - a0
+			for (var a0=max(0,r0-mx,c[0]-r1); a0<=mx; a0+=1) {
+				var a1 = r0-a0
 				if (a1 < 0) break
-				v.push( [a0, a1] ) // aggiunge [a0, a1] in fondo alla riga v
-			}
-			return v
-		}
-		for (var v=[], a0=max(0,c[0]-r1); a0<=mx; a0+=1) {
-			var a1 = r0 - a0 
-			if (a1 < 0) break
-			var vs = R(a1, shift1(c))
-			for (var i in vs) {
-				var vi = vs[i]
-				vi.unshift(a0) // aggiunge a0 in testa a vi
-				v.push(vi) // aggiunge vi in fondo a v
+				yield v.concat( a0, a1 ) // aggiunge [a0,a1] in fondo a v
 			}
 		}
-		return v
+		else {
+			for (var a0=max(0,c[0]-r1); a0<=mx; a0+=1) {
+				var r0n = r0-a0
+				yield* R(r0n, shift1(c), v.concat( a0 )) // aggiunge a0 in fondo a v
+			}
+		}
 	}
 }
 
 function M(mx, r, c) {
+	//console.log('M: ', mx, r, c) 
 	if (sum(r) != sum(c)) return
 	for (var mxr=mx*c.length, i=0; i<r.length; i+=1) if (r[i]> mxr) return 
 	for (var mxc=mx*r.length, i=0; i<c.length; i+=1) if (c[i]> mxc) return 
-	return M(r, c)
+	return M(r, c, [])
 	
-	function M(r, c) {
+	function* M(r, c, m) {
 		if (r.length == 2) { // 2 righe
+			yield* M2(r, c, m, [], [])
+		}
+		else { // più di 2 righe
+			for (var v=R(mx,r,c), vn=v.next(); !vn.done; vn=v.next()) {
+				var vi = vn.value
+				yield* M( shift1(r), subn(c, vi), m.concat( [vi] ) )
+			}
+		}
+		function* M2(r, c, m, r0, r1) {
 			if (c.length == 2) { // 2 righe, 2 colonne
-				for (var m=[], a00=max(0,c[0]-mx,r[0]-sum1(mx,c)); a00<=mx; a00+=1) {
+				for (var a00=max(0,c[0]-mx,r[0]-sum1(mx,c)); a00<=mx ; a00+=1) {
 					var a01 = r[0] - a00
 					if (a01 < 0) break
 					var a10 = c[0] - a00
 					if (a10 < 0) break
-					var a11 = r[1] - a10 // = c[1] - a01
+					var a11 = r[1] - a10 // = c[1] - a10
 					if (a11 < 0 || a11 > mx) break
- 					m.push( [[a00, a01], [a10, a11]] ) // genera la matrice 2x2
-				}
-				return m
-			}
-			// 2 righe, più di 2 colonne
-			for (var m=[], a0=max(0,c[0]-mx,r[0]-sum1(mx,c)); a0<=mx; a0+=1) {
-				var a1 = c[0] - a0
-				if (a1 < 0) break
-				var ms = M( [r[0]-a0, r[1]-a1], shift1(c) )
-				for (var i in ms) {
-					var mi = ms[i]
-					mi[0].unshift(a0) // aggiunge a0 in testa alla prima riga di mi
-					mi[1].unshift(a1) // aggiunge a1 in testa alla seconda riga di mi
-					m.push(mi) // aggiunge mi in fondo a m
+ 					yield m.concat( [r0.concat(a00, a01)], [r1.concat(a10, a11)] ) // aggiunge le ultime 2 colonne
 				}
 			}
-			return m
-		}
-		// più di 2 righe
-		for (var m=[], v=R(mx,r,c), i=0, e=v.length; i<e; i+=1) {
-			var vi = v[i]
-			var ms = M( shift1(r), subn(c, vi) )
-			for (var j in ms) {
-				var mj = ms[j]
-				mj.unshift(vi) // aggiunge vi in testa mj
-				m.push(mj) // aggiunge mj in fondo a m
+			else { // 2 righe, più di 2 colonne
+				for (var a0=max(0,c[0]-mx,r[0]-sum1(mx, c)); a0<=mx; a0+=1) {
+					var a1 = c[0] - a0
+					if (a1 < 0) break
+					yield* M2( [r[0]-a0, r[1]-a1], shift1(c), m, r0.concat(a0), r1.concat(a1) ) // aggiunge una colonna
+				}
 			}
 		}
-		return m
 	}
 }
 
 function Mn(mx, r, c) {
 	if (sum(r) != sum(c)) return
-	for (var mxr=mx*c.length, i=0; i<r.length; i+=1) if (r[i]> mxr) return 
-	for (var mxc=mx*r.length, i=0; i<c.length; i+=1) if (c[i]> mxc) return 
+	for (var mxr=mx*c.length, i=0; i<r.length; i+=1) if (r[i]> mxr) return
+	for (var mxc=mx*r.length, i=0; i<c.length; i+=1) if (c[i]> mxc) return
 	return Mn(r, c)
 	
 	function Mn(r, c) {
 		if (r.length == 2) {
 			if (c.length == 2) {
-				for (var n=0, a00=max(0,c[0]-mx,r[0]-sum1(mx,c)); a00<=mx ; a00+=1) {
+				for (var n=0, a00=max(0,c[0]-mx,r[0]-sum1(mx,c)); a00<=mx; a00+=1) {
 					var a01 = r[0] - a00
 					if (a01 < 0) break
 					var a10 = c[0] - a00
@@ -122,19 +109,19 @@ function Mn(mx, r, c) {
 			}
 			return n
 		}
-		for (var n=0, v=R(mx,r,c), i=0, e=v.length; i<e; i+=1) {
-			var vi = v[i]
+		for (var n=0, v=R(mx,r,c), nv=v.next(); !nv.done; nv=v.next()) {
+			var vi = nv.value
 			n += Mn( shift1(r), subn(c, vi) )
 		}
 		return n
 	}
 }
 
-// sostituisce la precedente, aggiunge il logging
+// sostituisce la precendete, aggiunge logging
 function Mn(mx, r, c) {
 	if (sum(r) != sum(c)) return
-	for (var mxr=mx*c.length, i=0; i<r.length; i+=1) if (r[i]> mxr) return 
-	for (var mxc=mx*r.length, i=0; i<c.length; i+=1) if (c[i]> mxc) return 
+	for (var mxr=mx*c.length, i=0; i<r.length; i+=1) if (r[i]> mxr) return
+	for (var mxc=mx*r.length, i=0; i<c.length; i+=1) if (c[i]> mxc) return
 	var n=0, inc=100000, lim=inc // counting, logging
 	Mn(r, c)
 	return n
@@ -153,7 +140,7 @@ function Mn(mx, r, c) {
 				}
 				return
 			}
-			// 2 righe, più di 2 colonne
+			// 2 righe, piu di 2 colonne
 			for (var a0=max(0,c[0]-mx,r[0]-sum1(mx,c)); a0<=mx; a0+=1) {
 				var a1 = c[0] - a0
 				if (a1 < 0) break
@@ -161,9 +148,9 @@ function Mn(mx, r, c) {
 			}
 			return
 		}
-		// più di 2 righe
-		for (var v=R(mx,r,c), i=0, e=v.length; i<e; i+=1) {
-			var vi = v[i]
+		// piu di 2 righe
+		for (var v=R(mx,r,c), nv=v.next(); !nv.done; nv=v.next()) {
+			var vi = nv.value
 			Mn( shift1(r), subn(c, vi) )
 			if (n > lim) lim += (writeN(lim), inc) // logging
 		}
@@ -193,6 +180,7 @@ if (typeof exports != 'undefined') {
 	exports.M = M
 	exports.Mn = Mn
 	exports.check = check
+	
 	function writeN(n) { // logging
 		console.log( '%d', n )
 	}
